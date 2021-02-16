049603c69084428e10b5f25c5ad2dc3e7cfcc0ea
AndyBeable <andy.beable@gmail.com>
2021-02-16 17:20:42 +0000

    approval card

--- react-components/src/index.js
+++ react-components/src/index.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail
-        author='Sam'
-        timeAgo='Today at 4:45PM'
-        text='Nice to meet you'
-        image='https://source.unsplash.com/random'
-      />
-      <CommentDetail
-        author='Alex'
-        timeAgo='Today at 2:00PM'
-        text='Hello there'
-        image='https://source.unsplash.com/random'
-      />
-      <CommentDetail
-        author='Iris'
-        timeAgo='Yesterday at 9:00A M'
-        text='Hola, que tal?'
-        image='https://source.unsplash.com/random'
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author='Sam'
+          timeAgo='Today at 4:45PM'
+          text='Nice to meet you'
+          image='https://source.unsplash.com/random'
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Alex'
+          timeAgo='Today at 2:00PM'
+          text='Hello there'
+          image='https://source.unsplash.com/random'
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Iris'
+          timeAgo='Yesterday at 9:00AM'
+          text='Hola, que tal?'
+          image='https://source.unsplash.com/random'
+        />
+      </ApprovalCard>
     </div>
   );
 };
