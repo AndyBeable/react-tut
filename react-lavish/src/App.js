43711cf23b35ed2887233ccbb3808abff0d58f59
AndyBeable <andy.beable@gmail.com>
2021-03-06 19:15:48 +0000

    lavish inital

---
+++ react-lavish/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
