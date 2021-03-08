1dc4fe05232ecdd226436db27d0eb0b0efd61901
AndyBeable <andy.beable@gmail.com>
2021-03-08 17:36:47 +0000

    about start

---
+++ react-port/src/components/About.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <>
+      <div className=''>
+        <div className='wrapper copy'>
+          <h3>About</h3>
+          <h1>Personal Info</h1>
+          <p>A little bit about me</p>
+        </div>
+        <div>
+          <div className='about__copy-container'>
+            <h3>Hello!</h3>
+            <p>
+              My name is Andy Beable and I'm a passionate Frontend Web Developer
+              using web technologies to build amazing products and focusing on
+              solving problems for different niches and different industries
+              using the power of technology.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About;
--- react-port/src/components/HeroSection.js
+++ react-port/src/components/HeroSection.js
@@ -8,14 +8,21 @@ function HeroSection() {
     <div className='hero-container'>
       {/* <video src='videos/video-1.mp4' autoPlay loop muted /> */}
       <h1>Andy Beable</h1>
-
+      <p>I am a Web Developer</p>
       <div class='hero-btns'>
         <Button
           className='btns'
           buttonStyle='btn--outline'
           buttonSize='btn--large'
         >
-          Get Started
+          Latest Projects
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+        >
+          More about me
         </Button>
       </div>
     </div>
