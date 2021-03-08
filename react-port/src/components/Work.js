ecf48ad44fa2e6c6acb36270466acd20bcadbc0a
AndyBeable <andy.beable@gmail.com>
2021-03-08 18:22:00 +0000

    work initial

---
+++ react-port/src/components/Work.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Work() {
+  return (
+    <>
+      <div className=''>
+        <div className='wrapper copy'>
+          <h3>Work</h3>
+          <h1>Latest Projects</h1>
+          <p>Here you can find some projects i've been working on recently.</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Work;
--- react-port/src/components/pages/Home.js
+++ react-port/src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
 import About from '../About';
+import Work from '../Work';
 
 function Home() {
   return (
     <>
       <HeroSection />
       <About />
+      <Work />
     </>
   );
 }
