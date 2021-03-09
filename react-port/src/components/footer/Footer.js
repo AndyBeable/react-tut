e27f8eee0b0090a058521b9d2cbe7ce27adcd640
AndyBeable <andy.beable@gmail.com>
2021-03-09 17:11:57 +0000

    footer update

--- react-port/src/components/footer/Footer.js
+++ react-port/src/components/footer/Footer.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import './Footer.css';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
 
 function Footer() {
   return (
     <>
-      <div className='footer'>
-        <div className='footer__container'></div>
-      </div>
+      <IconContext.Provider
+        value={{ color: '#fff', size: '2.3rem', className: 'social__icons' }}
+      >
+        <div className='footer'>
+          <div className='footer__container'>
+            <div className='social__container'>
+              <a href='https://github.com' target='_blank'>
+                <FaGithub />
+              </a>
+              <a
+                href='https://www.linkedin.com/in/andy-beable/'
+                target='_blank'
+              >
+                <FaLinkedin />
+              </a>
+            </div>
+            <div className='email'>
+              <a href='mailto:andy.beable@gmail.com'>andy.beable@gmail.com</a>
+            </div>
+          </div>
+        </div>
+      </IconContext.Provider>
     </>
   );
 }
