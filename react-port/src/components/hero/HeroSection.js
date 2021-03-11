f06b4a5353018ef8f3846997487ca868535bf914
AndyBeable <andy.beable@gmail.com>
2021-03-11 16:37:22 +0000

    responsive header skills

--- react-port/src/components/hero/HeroSection.js
+++ react-port/src/components/hero/HeroSection.js
@@ -7,24 +7,26 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       {/* <video src='videos/video-1.mp4' autoPlay loop muted />  */}
-      <h3>Hello!</h3>
-      <h1>Andy Beable</h1>
-      <p>I am a Web Developer.</p>
-      <div className='hero-btns'>
-        {/* <Button
+      <div className='hero-copy'>
+        <h3>Hello!</h3>
+        <h1>Andy Beable</h1>
+        <p>I am a Web Developer.</p>
+        <div className='hero-btns'>
+          {/* <Button
           className='btns'
           buttonStyle='btn--outline'
           buttonSize='btn--large'
         >
           Latest Projects
        </Button> */}
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-        >
-          More about me
-        </Button>
+          <Button
+            className='btns'
+            buttonStyle='btn--outline'
+            buttonSize='btn--large'
+          >
+            More about me
+          </Button>
+        </div>
       </div>
     </div>
   );
