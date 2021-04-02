c248ee3ca946f96d5f728e78c2678a228b6c4a9a
AndyBeable <andy.beable@gmail.com>
2021-04-02 19:46:56 +0100

    props

--- react-prac/src/index.js
+++ react-prac/src/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const firstBook = {
+  img: 'https://m.media-amazon.com/images/I/815TTLvJUFL._AC_UY218_.jpg',
+  title: 'They Book Die at the End',
+  author: 'Adam Silvera',
+};
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/81qqNcIoyiL._AC_UY218_.jpg',
+  title: 'American Dirt',
+  author: 'Jeanine Cummins',
+};
+
 function BookList() {
   return (
     <section className='book-list'>
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
       <Book />
       <Book />
       <Book />
@@ -15,15 +34,12 @@ function BookList() {
   );
 }
 
-const Book = () => {
-  const title = 'They Book Die at the End';
-  const author = 'Adam Silvera';
-
+const Book = props => {
   return (
     <article className='book'>
-      <img src='https://m.media-amazon.com/images/I/815TTLvJUFL._AC_UY218_.jpg' />
-      <h2>{title}</h2>
-      <h3>{author.toUpperCase()}</h3>
+      <img src={props.img} alt='book cover' />
+      <h2>{props.title}</h2>
+      <h3>{props.author}</h3>
     </article>
   );
 };
