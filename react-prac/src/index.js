56a5522ca2932bedbf7391b4f94d2d27eea00a80
AndyBeable <andy.beable@gmail.com>
2021-04-02 18:49:43 +0100

    styles

--- react-prac/src/index.js
+++ react-prac/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 function BookList() {
   return (
-    <section>
+    <section className='book-list'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
       <Book />
     </section>
   );
@@ -11,7 +17,7 @@ function BookList() {
 
 const Book = () => {
   return (
-    <article>
+    <article className='book'>
       <Image />
       <Title />
       <Author />
