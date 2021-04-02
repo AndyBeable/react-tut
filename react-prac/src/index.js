7f96048dfd9aba0cad1888ffe0f6dc078e625448
AndyBeable <andy.beable@gmail.com>
2021-04-02 17:54:19 +0100

    inital

---
+++ react-prac/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Greeting() {
+  return <h4>This is Andy.</h4>;
+}
+
+ReactDOM.render(<Greeting />, document.getElementById('root'));
