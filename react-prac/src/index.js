8dbbea3d4e31009b5c851f031d80f4252b417143
AndyBeable <andy.beable@gmail.com>
2021-04-02 18:37:55 +0100

    book first render

--- react-prac/src/index.js
+++ react-prac/src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Greeting() {
-  return <h4>This is Andy.</h4>;
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
 }
 
-ReactDOM.render(<Greeting />, document.getElementById('root'));
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img src='https://m.media-amazon.com/images/I/815TTLvJUFL._AC_UY218_.jpg' />
+);
+
+const Title = () => <h2>They Book Die at the End</h2>;
+const Author = () => <h3>Adam Silvera</h3>;
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
