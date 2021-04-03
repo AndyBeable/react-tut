8bf5c9c9bb93efde2ce502dbde38c1b1e888f811
AndyBeable <andy.beable@gmail.com>
2021-04-03 16:27:12 +0100

    props ext

--- react-prac/src/index.js
+++ react-prac/src/index.js
@@ -20,7 +20,13 @@ function BookList() {
         img={firstBook.img}
         title={firstBook.title}
         author={firstBook.author}
-      />
+      >
+        <p>
+          Some text here. Some text here. Some text here. Some text here. Some
+          text here. Some text here. Some text here. Some text here. Some text
+          here.
+        </p>
+      </Book>
       <Book
         img={secondBook.img}
         title={secondBook.title}
@@ -35,11 +41,16 @@ function BookList() {
 }
 
 const Book = props => {
+  // can also destructure props ({ img, title, author, children}})
+  // or can destructure like by passing props in to variable
+  // const {img, title, author, children} = props;
+
   return (
     <article className='book'>
       <img src={props.img} alt='book cover' />
       <h2>{props.title}</h2>
       <h3>{props.author}</h3>
+      {props.children}
     </article>
   );
 };
