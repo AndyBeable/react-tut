8c5086a0581193ca427888569b479941fb29bbe2
AndyBeable <andy.beable@gmail.com>
2021-04-03 17:43:05 +0100

    map

--- react-prac/src/index.js
+++ react-prac/src/index.js
@@ -2,41 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/815TTLvJUFL._AC_UY218_.jpg',
-  title: 'They Book Die at the End',
-  author: 'Adam Silvera',
-};
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/81qqNcIoyiL._AC_UY218_.jpg',
-  title: 'American Dirt',
-  author: 'Jeanine Cummins',
-};
+const books = [
+  {
+    img: 'https://m.media-amazon.com/images/I/815TTLvJUFL._AC_UY218_.jpg',
+    title: 'They Book Die at the End',
+    author: 'Adam Silvera',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/81qqNcIoyiL._AC_UY218_.jpg',
+    title: 'American Dirt',
+    author: 'Jeanine Cummins',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/81I8+1VJrML._AC_UY218_.jpg',
+    title: 'The Madness of Grief',
+    author: 'Richard Coles',
+  },
+];
 
 function BookList() {
   return (
     <section className='book-list'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map(book => {
+        const { img, title, author } = book;
+        return <Book book={book}></Book>;
+      })}
     </section>
   );
 }
 
 const Book = props => {
-  const { img, title, author } = props;
+  const { img, title, author } = props.book;
   return (
     <article className='book'>
       <img src={img} alt='book cover' />
